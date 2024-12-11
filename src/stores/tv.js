0816fa4a7f7d6123351227417449d5353a1fefbe
lucas <lucasdesouzasouza57@gmail.com>
2024-12-11 11:21:46 -0300

    FEAT: atualização da pagina de programas de tv

--- src/stores/tv.js
+++ src/stores/tv.js
@@ -2,15 +2,15 @@ import { reactive, computed } from 'vue';
 import { defineStore } from 'pinia';
 import api from '@/plugins/axios';
 
-export const useTvStore = defineStore('Tv', () => {
+export const useTvStore = defineStore('programa', () => {
   const state = reactive({
     currentTv: {},
   });
 
   const currentTv = computed(() => state.currentTv);
 
-  const getTvDetail = async (tvId) => {
-    const response = await api.get(`Tv/${tvId}`);
+  const getTvDetail = async (programaId) => {
+    const response = await api.get(`programa/${programaId}`);
     state.currentTv = response.data;
   };
 
