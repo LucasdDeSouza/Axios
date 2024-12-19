e8b9859f80a722d9ed6a7842f5b7ee2124288467
higoramaral <amarahigor@gmail.com>
2024-12-19 10:53:15 -0300

    FEAT: atualizar visual e funcionalidade das páginas de detalhes de TV e filmes

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,107 +1,101 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
-  import api from '@/plugins/axios';
-  import { useGenreStore } from '@/stores/genre';
-  import { useRouter } from 'vue-router'
+import { ref, onMounted } from 'vue'
+import Loading from 'vue-loading-overlay'
+import api from '@/plugins/axios'
+import genreStore from '@/stores/genre'
+import { useRouter } from 'vue-router'
 const router = useRouter()
-
-  import Loading from 'vue-loading-overlay';
-
-  const isLoading = ref(false);
-
-
-  const genreStore = useGenreStore();
-
-  const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
-
-
-
-  onMounted(async () => {
-  isLoading.value = true;
-  await genreStore.getAllGenres('movie');
-  isLoading.value = false;
-});
-
-  onMounted(async () => {
-  isLoading.value = true;
-  await genreStore.getAllGenres('movie');
-  isLoading.value = false;
-});
-
-  const movies = ref([]);
-
-  const listMovies = async (genreId) => {
-  genreStore.setCurrentGenreId(genreId);
-  isLoading.value = true;
-  const response = await api.get('discover/movie', {
-    params: {
-      with_genres: genreId,
-      language: 'pt-BR',
-    },
-  });
-  movies.value = response.data.results;
-  isLoading.value = false;
-};
 
 function openMovie(movieId) {
   router.push({ name: 'MovieDetails', params: { movieId } });
-} 
+}
 
+const isLoading = ref(false)
+onMounted(async () => {
+  isLoading.value = true
+  await genreStore.getAllGenres('movie')
+  isLoading.value = false
+})
 
+const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
+
+const movies = ref([])
+
+const listMovies = async (genreId) => {
+  genreStore.setCurrentGenreId(genreId);
+  isLoading.value = true
+  const response = await api.get('discover/movie', {
+    params: {
+      with_genres: genreId,
+      language: 'pt-BR'
+    }
+  })
+  movies.value = response.data.results
+  isLoading.value = false
+}
 </script>
-
 <template>
-  <h1>Filmes</h1>
-  <ul class="genre-list">
-    <li
-    v-for="genre in genreStore.genres"
-    :key="genre.id"
-    @click="listMovies(genre.id)"
-    class="genre-item"
-    :class="{ active: genre.id === genreStore.currentGenreId }"
-  >
+  <div class="movies-container">
+    <h1 class="hero-title">Descubra Novos Filmes</h1>
+    <ul class="genre-list">
+      <li
+        v-for="genre in genreStore.genres"
+        :key="genre.id"
+        @click="listMovies(genre.id)"
+        class="genre-item"
+      >
+        {{ genre.name }}
+      </li>
+    </ul>
 
-    {{ genre.name }}
+    <loading v-model:active="isLoading" is-full-page />
 
-  </li>
-  </ul>
-
-  <loading v-model:active="isLoading" is-full-page />
-
-  <div class="movie-list">
-  <div v-for="movie in movies" :key="movie.id" class="movie-card">
-    <img
+    <div class="movie-list">
+      <div v-for="movie in movies" :key="movie.id" class="movie-card">
+        <img
   :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
   :alt="movie.title"
   @click="openMovie(movie.id)"
-/>
-    <div class="movie-details">
-      <p class="movie-title">{{ movie.title }}</p>
-      <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
-      <p class="movie-genres">
-        <span
-  v-for="genre_id in movie.genre_ids"
-  :key="genre_id"
-  @click="listMovies(genre_id)"
-  :class="{ active: genre_id === genreStore.currentGenreId }"
->
-   {{ genreStore.getGenreName(genre_id) }}
-</span>
-
-      </p>
+/>   
+        <div class="movie-details">
+          <p class="movie-title">{{ movie.title }}</p>
+          <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
+          <div class="movie-genres">
+            <span
+              v-for="genre_id in movie.genre_ids"
+              :key="genre_id"
+              @click="listMovies(genre_id)"
+              class="genre-tag"
+            >
+              {{ genreStore.getGenreName(genre_id) }}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
-
   </div>
-</div>
-
-
 </template>
+
 <style scoped>
+.movies-container {
+  text-align: center;
+  padding: 20px;
+  background-color: #101010;
+  color: #fff;
+  
+}
+
+.hero-title {
+  font-size: 3rem;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
 .genre-list {
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-  gap: 2rem;
+  gap: 1rem;
   list-style: none;
   margin-bottom: 2rem;
 }
@@ -111,10 +105,11 @@ function openMovie(movieId) {
   border-radius: 1rem;
   padding: 0.5rem 1rem;
   color: #fff;
+  transition: background-color 0.3s ease;
+  cursor: pointer;
 }
 
 .genre-item:hover {
-  cursor: pointer;
   background-color: #4e9e5f;
   box-shadow: 0 0 0.5rem #387250;
 }
@@ -122,66 +117,74 @@ function openMovie(movieId) {
   display: flex;
   flex-wrap: wrap;
   gap: 1rem;
+  justify-content: center;
 }
 
 .movie-card {
   width: 15rem;
   height: 30rem;
-  border-radius: 0.5rem;
+  border-radius: 1rem;
   overflow: hidden;
-  box-shadow: 0 0 0.5rem #000;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.3);
+  transition: transform 0.3s ease-in-out;
+  background-color: #202020;
+}
+
+.movie-card:hover {
+  transform: scale(1.05);
 }
 
 .movie-card img {
   width: 100%;
   height: 20rem;
-  border-radius: 0.5rem;
-  box-shadow: 0 0 0.5rem #000;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.3);
 }
 
 .movie-details {
-  padding: 0 0.5rem;
+  padding: 1rem;
 }
 
 .movie-title {
-  font-size: 1.1rem;
+  font-size: 0.9rem;
   font-weight: bold;
-  line-height: 1.3rem;
-  height: 3.2rem;
+  line-height: 1.5;
+  height: 3.5rem;
+  margin-bottom: 0.5rem;
+}
+.movie-release-date {
+
+  font-size: 1rem;
+ 
+}
+
+.movie-release-date:hover {
+  color: var(--primary-color);
 }
 
 .movie-genres {
   display: flex;
-  flex-direction: row;
   flex-wrap: wrap;
-  align-items: flex-start;
+  gap: 0.5rem;
+  margin-top: 1rem;
   justify-content: center;
-  gap: 0.2rem;
 }
 
-.movie-genres span {
-  background-color: #748708;
+.genre-tag {
+  background-color: #086387;
   border-radius: 0.5rem;
   padding: 0.2rem 0.5rem;
   color: #fff;
   font-size: 0.8rem;
   font-weight: bold;
+  transition: background-color 0.3s ease;
+  cursor: pointer;
 }
 
-.movie-genres span:hover {
-  cursor: pointer;
+.genre-tag:hover {
   background-color: #455a08;
   box-shadow: 0 0 0.5rem #748708;
 }
 
-.active {
-  background-color: #67b086;
-  font-weight: bolder;
-}
-
-.movie-genres span.active {
-  background-color: #abc322;
-  color: #000;
-  font-weight: bolder;
-}
 </style>
--- src/views/TvDetailsView.vue
+++ src/views/TvDetailsView.vue
@@ -1,62 +1,139 @@
 <script setup>
-import { defineProps, onMounted } from 'vue';
+import { onMounted } from 'vue';
 import { useTvStore } from '@/stores/tv';
+
 const tvStore = useTvStore();
 
 const props = defineProps({
-  movieId: {
-    type: Number,
-    required: true,
-  },
+    tvId: {
+        type: String,
+        required: true,
+    },
 });
 
 onMounted(async () => {
-  await tvStore.getTvDetail(props.programaId);
+    await tvStore.getTvDetail(props.tvId);
 });
+
 </script>
 
 <template>
-
-<div class="main">
-  <div class="content">
-    <img
-      :src="`https://image.tmdb.org/t/p/w185${tvStore.currentTv.poster_path}`"
-      :alt="tvStore.currentTv.name"
-    />
-
-    <div class="details">
-      <h1>Filme: {{ tvStore.currentTv.name }}</h1>
-      <p>{{ tvStore.currentTv.tagline }}</p>
-      <p>{{ tvStore.currentTv.overview }}</p>
-      <p>Orçamento: ${{ tvStore.currentTv.budget }}</p>
-      <p>Avaliação: {{ tvStore.currentTv.vote_average }}</p>
+    <div class="tv-details">
+      <div class="content">
+        <img
+          :src="`https://image.tmdb.org/t/p/w185${tvStore.currentTv.poster_path}`"
+          :alt="tvStore.currentTv.name"
+          class="poster-image"
+        />
+  
+        <div class="details">
+          <h1 class="tv-title">{{ tvStore.currentTv.name }}</h1>
+          <p class="tagline">{{ tvStore.currentTv.tagline }}</p>
+          <p class="overview">{{ tvStore.currentTv.overview }}</p>
+          <p class="rating">Avaliação: {{ tvStore.currentTv.vote_average }}</p>
+        </div>
+      </div>
+  
+      <div class="production-companies">
+        <p class="companies-title">Produtoras</p>
+        <div class="companies-list">
+          <template v-for="company in tvStore.currentTv.production_companies" :key="company.id">
+            <div class="company">
+              <img
+                v-if="company.logo_path"
+                :src="`https://image.tmdb.org/t/p/w92${company.logo_path}`"
+                :alt="company.name"
+                class="company-logo"
+              />
+              <p v-else class="company-name">{{ company.name }}</p>
+            </div>
+          </template>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
-<p>Produtoras</p>
-<div class="companies">
-  <template
-    v-for="company in tvStore.currentTv.production_companies"
-    :key="company.id"
-  >
-    <img
-      v-if="company.logo_path"
-      :src="`https://image.tmdb.org/t/p/w92${company.logo_path}`"
-      :alt="company.name"
-    />
-    <p v-else>{{ company.name }}</p>
   </template>
-</div>
-
-</template>
-
-<style scoped>
-.companies {
-  display: flex;
-  flex-direction: row;
-  column-gap: 3rem;
-  align-items: center;
-  margin-bottom: 2rem;
-}
-</style>
+  
+  <style scoped>
+  .tv-details {
+    text-align: center;
+    padding: 20px;
+    background-color: #3d3d3d;
+    color: #fff;
+  }
+  
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  
+  .poster-image {
+    width: 185px;
+    height: auto;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(175, 175, 175, 0.2);
+    margin-right: 20px;
+  }
+  
+  .details {
+    max-width: 600px;
+  }
+  
+  .tv-title {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 10px;
+    color: #ffd700; 
+  }
+  
+  .tagline {
+    font-style: italic;
+    color: #777;
+    margin-bottom: 10px;
+  }
+  
+  .overview {
+    margin-bottom: 10px;
+  }
+  
+  .rating {
+    font-weight: bold;
+    color: #00ff00; 
+  }
+  
+  .production-companies {
+    margin-top: 20px;
+  }
+  
+  .companies-title {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-bottom: 10px;
+    color: #ffd700; 
+  }
+  
+  .companies-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  
+  .company {
+    margin-bottom: 10px;
+  }
+  
+  .company-logo {
+    width: 92px;
+    height: auto;
+    margin-right: 10px;
+    border-radius: 4px;
+    box-shadow: 0 0 5px rgb(214, 214, 214);
+  }
+  
+  .company-name {
+    margin-bottom: 10px;
+    color: #fff; 
+  }
+  </style>
+  
